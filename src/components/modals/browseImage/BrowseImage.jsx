443965fee825c7eac23c0eaf25003c378daadb00
Murtuza Chakkiwala <>
2021-07-12 13:55:12 +0530

    added camera option

--- src/components/modals/browseImage/BrowseImage.jsx
+++ src/components/modals/browseImage/BrowseImage.jsx
@@ -7,12 +7,21 @@ const BrowseImage = ({ onImageSelect }) => {
 
     return (<div className="browse-image-container d-flex flex-column align-items-center justify-content-center">
         <img src={browseDevice} alt="Browse Device" width="50%" className="mt-5 pt-5" />
-        <div className="d-flex justify-content-center mt-5">
+        <div className="d-flex justify-content-center mt-5 flex-column">
             <label className="btn btn-default p-0 w-auto">
-                <div className="upload-button pl-5 pr-5 image-select-button browseBtn">Browse Photos</div>
-                <input type="file" accept="image/png, image/gif, image/jpeg" hidden onChange={(data) => {
-                    onImageSelect(data.target.files[0], 'FROM_DEVICE');
-                }} />
+                <div className="upload-button pl-5 pr-5 image-select-button browseBtn">From Gallery</div>
+                <input type="file" accept="image/*"
+                    hidden onChange={(data) => {
+                        onImageSelect(data.target.files[0], 'FROM_DEVICE');
+                    }} />
+            </label>
+
+            <label className="btn btn-default p-0 w-auto">
+                <div className="upload-button pl-5 pr-5 image-select-button browseBtn">From Camera</div>
+                <input type="file" accept="image/*"
+                    capture hidden onChange={(data) => {
+                        onImageSelect(data.target.files[0], 'FROM_DEVICE');
+                    }} />
             </label>
         </div>
     </div>);
